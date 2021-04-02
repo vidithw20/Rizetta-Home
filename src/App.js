7cc668baba3f93ddf9badad1b81975ede9567c4e
vidithw20 <77677864+vidithw20@users.noreply.github.com>
2021-04-03 02:25:57 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./Components/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
